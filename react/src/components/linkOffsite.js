4bcd5904910764f87c3ab56109c7648c5cd12309
ties <tiesnoordhuis@gmail.com>
2019-06-17 18:40:16 +0200

    alligned link box

--- react/src/components/linkOffsite.js
+++ react/src/components/linkOffsite.js
@@ -5,16 +5,14 @@ import Row from "react-bootstrap/Row";
 export default class LinkOffsite extends React.Component {
     render() {
       var style = {
-        "background-image": `
-          linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)),
-          url(${this.props.linkObject.background})
-        `,
+        "background-image": `linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)),
+          url(${this.props.linkObject.background})`,
       }
 
       return (
         <Col xs={12} md={6}>
           <Row>
-            <Col xs={{ span: 10, offset: 1 }} style={style} className={"linkObject m-3 p-0"}>
+            <Col xs={{ span: 10, offset: 1 }} style={style} className={"linkObject my-3 p-0"}>
               <div className={"p-4 m-4 display-4 text-center"}>
                 <a href={this.props.linkObject.url} className={"linkUrl"}>
                   {this.props.linkObject.title}
--- react/src/components/main.js
+++ react/src/components/main.js
@@ -1,34 +1,12 @@
 import React from "react"
 import Links from "./links"
-import Jumbotron from "react-bootstrap/Jumbotron"
+import MainContent from "./mainContent"
 
 export default class Main extends React.Component {
     render() {
       return (
         <div className="container-fluid">
-            <div className="row">
-            <Jumbotron className="rounded-5 mx-auto my-auto">
-                <div class="container">
-                    <h1 class="display-4 text-center">Annotech Groep</h1>
-                    <br/>
-                    <p class="lead">
-                        Annotech is een groep van van gecertificeerde technische bedrijven
-                        <br/>
-                        werkzaam in de middel grote en zware industrie
-                        <ul>
-                        <li>productie van machines, machineonderdelen</li>
-                        <li>gespecialiseerd laswerk</li>
-                        <li>gespecialiseerd in toepassing van slijtvaste materialen en laswerk</li>
-                        <li>uitvoeren van grotere totaal projecten inclusief projectmanagement</li>
-                        <li>onderhoud aan machines</li>
-                        <li>onderhoud op basis van offerte, in werkplaats en op locatie</li>
-                        <li>contract onderhoud, in werkplaats en op locatie</li>
-                        </ul>
-                    </p>
-                    
-                </div>
-            </Jumbotron>
-            </div>
+            <MainContent />
             <Links />
         </div>
       );
